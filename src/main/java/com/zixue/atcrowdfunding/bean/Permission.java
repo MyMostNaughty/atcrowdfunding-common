dcf463dac1adfc19df19b4f865681b7677664559
hepan <910007372@qq.com>
2020-06-01 01:24:42 +0800

    完善 非法访问和登录校验

--- src/main/java/com/zixue/atcrowdfunding/bean/Permission.java
+++ src/main/java/com/zixue/atcrowdfunding/bean/Permission.java
@@ -10,8 +10,22 @@ public class Permission {
 	private String url;
 	private Integer pid;
 	private boolean open = true;
+	private boolean checked = false;
+	private String icon;
 	private List<Permission> children = new ArrayList<Permission>();
 	
+	public boolean isChecked() {
+		return checked;
+	}
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
 	public Integer getId() {
 		return id;
 	}
